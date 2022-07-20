52bdf9d6168ebb4f6a4f78f8e6e6e06b4edde2ae
claudiu-cc <claudiu@cyber-crypt.com>
2022-07-20 10:04:52 +0200

    Added godoc comments for migration

--- migration/options.go
+++ migration/options.go
@@ -6,6 +6,7 @@ type options struct {
 	batchSize int
 }
 
+// Option is used to configure optional settings for the migration.
 type Option func(*options)
 
 func defaultOptions() options {
@@ -20,6 +21,7 @@ func (o *options) apply(opts ...Option) {
 	}
 }
 
+// WithBatchSize sets the batch size to be used when migrating data. The default batch size is 10.
 func WithBatchSize(batchSize int) Option {
 	return func(o *options) {
 		o.batchSize = batchSize
